e78ca1e310541a6e1fee6dbd46dfb6647ae3eb48
Seonaid Mc <seonaidmcnabb@gmail.com>
2022-11-13 16:00:55 +0100

    raise val error

--- student.py
+++ student.py
@@ -1,5 +1,16 @@
+"""
+Practicing OOP concepts with Python
+"""
+#Initializes an otherwise empty object when you create it.
 class Student:
+    """
+    Commenting
+    """
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "HufflePuff","Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
@@ -8,10 +19,9 @@ def main():
     print(f'{student.name} is from {student.house}')
 
 def get_student():
-   name = input("Name: ")
-   house = input("House: ")
-   student = Student(name, house)
-   return student
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
