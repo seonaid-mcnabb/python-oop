f558e606f5ec011c899e9f5af3bd5d697288d3c2
Seonaid Mc <seonaidmcnabb@gmail.com>
2022-11-13 16:43:36 +0100

    use getter and setter

--- student.py
+++ student.py
@@ -6,36 +6,32 @@ class Student:
     """
     Commenting
     """
-    def __init__(self, name, house, patronus):
+    def __init__(self, name, house):
         if not name:
             raise ValueError("Missing name")
-        if house not in ["Gryffindor", "HufflePuff","Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
         self.name = name
         self.house = house
-        self.patronus = patronus
     
     def __str__(self):
-        return f'{self.name} is from {self.house} and their patronus is {self.patronus}'
+        return f'{self.name} is from {self.house}'
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
     
-    def charm(self):
-        match self.patronus:
-            case "Stag":
-                return "Horse"
-            case "Otter":
-                return "Otter"
-            case "Jack Russell Terrier":
-                return "Bar"
-            case _:
-                return "Wand with no magic"
 
 def main():
     """
     Commenting
     """
     student = get_student()
-    print("Expecto patronum!")
-    print(student.charm())
+    print(student)
 
 def get_student():
     """
@@ -43,8 +39,7 @@ def get_student():
     """
     name = input("Name: ")
     house = input("House: ")
-    patronus = input ("Patronus: ")
-    return Student(name, house, patronus)
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
